eb1b95542fea7376d14382f49ffb68ee0f7eac5d
b3ast31 <80463448+b3ast31@users.noreply.github.com>
2022-08-05 10:49:26 +0700

    feat(listview): make a list of bandung tourist object and details

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tempat_wisata_bandung/detail_screen.dart';
+import 'package:tempat_wisata_bandung/main_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const DetailScreen(),
+      home: const MainScreen(),
     );
   }
 }
